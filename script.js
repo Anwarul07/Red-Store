eee158392b6b6cd795c9a780f3f30b900ac7df84
Anwarul07 <Officialmdanwarulhaque@gmail.com>
2024-12-22 00:31:32 +0530

    Update script.js

--- script.js
+++ script.js
@@ -9,3 +9,21 @@ function toggle() {
     }
 }
 
+// Product image section 
+let ProductImg = document.getElementById("product-img")
+let SmallImg = document.getElementsByClassName("small-img");
+
+SmallImg[0].addEventListener("click", function () {
+    ProductImg.src = SmallImg[0].src;
+})
+SmallImg[1].addEventListener("click", function () {
+    ProductImg.src = SmallImg[1].src;
+})
+SmallImg[2].addEventListener("click", function () {
+    ProductImg.src = SmallImg[2].src;
+})
+SmallImg[3].addEventListener("click", function () {
+    ProductImg.src = SmallImg[0].src;
+})
+
+
